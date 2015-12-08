ad5c3436c70bd2bcaebd0d997ceb4509f5d23074
tommy.hilfiger <tommy.hilfiger@daumkakao.com>
2015-12-08 10:42:40 +0900

    new

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+
+object Main extends JFXApp {
+
+}
